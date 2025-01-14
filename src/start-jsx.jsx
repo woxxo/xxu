4ef116910d0cc76d92a784c61320ed6349e4754a
woxxo <lesf1@hotmail.com>
2025-01-14 23:35:56 +0300

    0.2.1

--- src/start-jsx.jsx
+++ src/start-jsx.jsx
@@ -1,36 +1,23 @@
 import { Xxu } from 'xxu';
 import { renderToReadableStream } from 'xxu/reacc';
 
-// const xxu = new Xxu(8811);
-// const xxu = new Xxu(8811, () => 'Hello, XXU!');
-
-//await Bun.$`pwd`;
-
-const xxu = new Xxu(8811, (method, path, headers) => {
-	switch (path) {
-		case '/teapot':
-			return new Response(`I'm a teapot`, {
-				status: 418,
-				headers: {
-					'Content-Type': 'text/html; charset=utf-8',
-					'Set-Cookie': `teapot=false; SameSite=Lax; path=/`,
-				},
-			});
-			//break;
-		case '/picture':
-			return Bun.file('./public_html/bun.jpg');
-			//break;
-		default:	
-			return `
-				<!DOCTYPE html>
-				<html>
-				<body>
-				<h2>from START:</h2>
-				${method}<br />${path}<br />${JSON.stringify(headers)}<br /><img src="/picture" alt="bun" />
-				</body>
-				</html>`;
-	}
+const xxu = new Xxu(8811, async (method, path, headers) => {
+	return await renderToReadableStream(
+		<html lang="en">
+		<head>
+			<meta charset="utf-8" />
+			<meta name="description" content="Default description" />
+			<title>Default</title>
+			<style>{ /* css */ `body {font-family: sans-serif; line-height: 1.5;}`}</style>
+		</head>
+		<body>
+			{ new Array(10).fill(0).map(_ => <p key={_} style={{ padding: "5px" }}>{_}</p>) }
+		</body>
+		</html>, {
+		bootstrapScriptContent: 'alert(1);',
+		bootstrapScripts: ['/main1.js', '/main2.js', ],
+	});
 });
 
 
-console.log(`Hello from START! Running "${xxu.version}" on ${xxu.hostname}:${xxu.port}`);
+console.log(`Hello from START-JSX! Running "${xxu.version}" on ${xxu.hostname}:${xxu.port}`);
